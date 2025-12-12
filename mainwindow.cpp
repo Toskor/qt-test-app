f89c502042c75ef42a05426798ee5cc5a8e5b9c6
Toskor <anitobor@gmail.com>
2025-12-12 13:08:58 +0300

    some functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QApplication>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    
+    // Set fixed window size
+    setFixedSize(400, 300);
+    
+    // Connect exit button to close application
+    connect(ui->exitButton, &QPushButton::clicked, this, &QApplication::quit);
 }
 
 MainWindow::~MainWindow()
